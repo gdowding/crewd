f40426dfff3c519b48eb2df4d718a3cd7a374000
George Dowding <pyrios@gmail.com>
2026-02-01 23:19:35 -0800

    debug reading from csv
    header was missing but didn't see error when running or building
    browser code.

--- crewd-f/src/main.rs
+++ crewd-f/src/main.rs
@@ -26,6 +26,7 @@ fn App() -> impl IntoView {
     // changed csv format to have matching header values
     //let file_path = "/Users/gdowding/git/github/gdowding/crewd/schedule.csv";
     let data = "\
+date_start,date_end,series,race,sponsor,event_page
 2026-03-14,,Snowbird Race Series,Race 5,styc,https://www.styc.org/Snowbird-Series
 2026-03-21,,Center Sound Series,CSS#3 - Possession Point Race,cyc seattle,https://cycseattle.theclubspot.com/regatta/M3yLIpxRaJ
 2026-04-11,,Blakely Rock Benefit Race,Race 1,styc,https://www.styc.org/Blakely-Rock-Benefit-Race
@@ -35,20 +36,20 @@ fn App() -> impl IntoView {
 2026-04-27,,Ballard Cup I,Race 3,styc,https://www.styc.org/Ballard-Cup-Series-I
 ";
 
-    let mut events: Vec<csv::StringRecord> = vec![];
+    // let mut events: Vec<csv::StringRecord> = vec![];
     let mut rdr = Reader::from_reader(data.as_bytes());
-    for record in rdr.records() {
-	let event = record.unwrap();
-	//println!(event);
-	events.push(event);
-    }
-	    
-    //let mut rdr = Reader::from_path(file_path).unwrap();
 
-    /*
+    // for record in rdr.records() {
+    // 	let event = record.unwrap();
+    // 	//println!(event);
+    // 	events.push(event);
+    // }
+
+    //let mut rdr = Reader::from_path(file_path).unwrap();
     let mut events: Vec<Event> = Vec::new();
     for result in rdr.deserialize() {
 	let event: Event = result.unwrap();
+	println!("event: {:?}", event);
 	events.push(event);
     }
     let races :Vec<String> = events
@@ -56,14 +57,13 @@ fn App() -> impl IntoView {
         .map(|event| event.race.clone())
         .collect();
 
-     */
-    
-    let races :Vec<String> = events
-        .iter()
-        .map(|event| event.get(3).expect("reason").to_string())
-        .collect();
 
-    
+    // let races :Vec<String> = events
+    //     .iter()
+    //     .map(|event| event.get(3).expect("reason").to_string())
+    //     .collect();
+
+
     view! {
 	<ul>
 	{
@@ -75,4 +75,3 @@ fn App() -> impl IntoView {
     }
 
 }
-
