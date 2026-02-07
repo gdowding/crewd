2cdf1498282341affc42a14a3b9a8a287aa6e6f5
George Dowding <pyrios@gmail.com>
2026-02-07 13:19:22 -0800

    call server function and return vec of structs
    Able to call fun and receive a vec of structs, then extract if it is
    complete and then determine if there is an error or not. If there is
    no error, extract the results.

--- crewd/src/app.rs
+++ crewd/src/app.rs
@@ -5,6 +5,7 @@ use leptos_router::{
     StaticSegment,
 };
 
+use serde::{Deserialize, Serialize};
 
 #[cfg(feature = "ssr")]
 use tokio::time::{sleep, Duration};
@@ -57,39 +58,73 @@ pub fn App() -> impl IntoView {
 }
 
 
+
+
 #[server]
-pub async fn get_schedule(n: i32) -> Result<String, ServerFnError> {
-    log!("get_schedule");
+pub async fn get_schedule_string(n: i32) -> Result<String, ServerFnError> {
+    log!("get_schedule_string");
     sleep(Duration::from_secs(1)).await;
     log!("after sleep");
     Ok(format!("schedule string '{n}'"))
-    //Err(ServerFnError::ServerError("server error".to_string()))
 }
 
+#[server]
+pub async fn get_events() -> Result<Vec<Event>, ServerFnError> {
+
+    log!("get_events");
+    let events = vec![
+	Event{
+	    key: "sfoo".to_string(),
+	    name: "race 4".to_string(),
+	},
+	Event{
+	    key: "sbar".to_string(),
+	    name: "race 5".to_string(),
+	},
+	Event{
+	    key: "sbaz".to_string(),
+	    name: "race 6".to_string(),
+	},
+    ];
+    Ok(events)
+}
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Event {
+    key: String,
+    name: String,
+}
 
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
     log!("begin homepage");
-    // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
 
-
-    // let sched_data = move || { match sched_resource.get() {
-    // 	None => view! {"Loading"}.into_any(),
-    // 	Some(schedule) => view!{ schedule }.into_any()
-    // }};
+    let (events, _set_events) = signal(vec![
+	Event{
+	    key: "foo".to_string(),
+	    name: "race 1".to_string(),
+	},
+	Event{
+	    key: "bar".to_string(),
+	    name: "race 2".to_string(),
+	},
+	Event{
+	    key: "baz".to_string(),
+	    name: "race 3".to_string(),
+	},
+    ]);
 
     let s_count = RwSignal::new(0);
-    let sched_resource = Resource::new(move || s_count, |n| get_schedule(n.get()));
+    let sched_resource = Resource::new(move || s_count, |n| get_schedule_string(n.get()));
     let reload_schedule = move |_| *s_count.write() += 1;
 
-    view! {
-        <h1>"Welcome to Leptos!"</h1>
-            <button on:click=on_click>"Click Me: " {count}</button>
-	    <br/>
+    // server events
+    let events_r = Resource::new(move || (), |_| get_events());
 
+    view! {
+        <h1>"Welcome to Crewd"</h1>
 	    <button on:click=reload_schedule>Reload Schedule</button>
 	    <br/>
 	<Suspense
@@ -98,6 +133,31 @@ fn HomePage() -> impl IntoView {
 	{sched_resource}
         </Suspense>
 	    <p>"after suspense"</p>
+	    <For
+	    each=move || events.get()
+	    key=|event| event.key.clone()
+	    let(event)
+	    >
+	    <p>{event.name}</p>
+	    </For>
+
+	    <h2>server events</h2>
+	<Suspense
+	    fallback=move || view! {<p>"Loading..."</p>}
+	>
+	{move ||
+	 match events_r.get() {
+	     None => view! {<p>"got nothing"</p>}.into_any(),
+	     Some(result) =>
+		 match result {
+		     Ok(events) => events.into_iter().map(move |event| view! {<p>{event.name}</p>}).collect::<Vec<_>>().into_any(),
+		     Err(e) => view! { <p>"got error"</p> }.into_any()
+		 }
+	 }
+	}
+	    </Suspense>
+
+
 
     }
 }
