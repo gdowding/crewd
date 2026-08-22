9b1d76f126f1295c33c6f684a7d30b6b9f4cf681
George Dowding <pyrios@gmail.com>
2026-08-22 13:34:37 -0700

    filter events that have not yet occoured

--- src/crewd.hs
+++ src/crewd.hs
@@ -1,12 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main (main) where
 
 import System.IO
+import System.Exit as Exit
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BL
 import Data.Char (isSpace)
+import Data.Csv
 import Data.List (intercalate, dropWhileEnd)
 import Data.List.Split (splitOn)
+import Data.Time.Clock (getCurrentTime, utctDay)
+import Data.Text (Text, pack, unpack)
+import Data.Time.Calendar (Day, toGregorian)
+import Data.Time.Format (defaultTimeLocale,  parseTimeM)
+import qualified Data.Vector as V
+import GHC.Generics (Generic)
 import Network.HTTP
 import Text.HTML.TagSoup
 
+
+data Event = Event
+  { regatta    :: !Text
+  , series     :: !Text
+  , start_date :: !Day
+  , race       :: !Text
+  } deriving (Show, Generic)
+
+parseDate :: BS.ByteString -> Parser Day
+parseDate s =
+  case parseTimeM True defaultTimeLocale "%Y-%m-%d" (BC.unpack s) of
+    Just day -> pure day
+    Nothing -> fail $ "Could not parse ISO-8601 date: " ++ BC.unpack s
+
+instance FromField Day where
+  parseField = parseDate
+
+
+instance FromNamedRecord Event
 
 
 trimEnd :: String -> String
@@ -14,10 +47,10 @@ trimEnd = dropWhileEnd isSpace
 
 -- Replace embedded " with "" and wrap entire string in double quotes.
 escapeCSV :: [Char] -> [Char]
-escapeCSV s = (wrapDoubleQuote . escapeQuotes) s
+escapeCSV = wrapDoubleQuote . escapeQuotes
   where
     wrapDoubleQuote s' = "\"" ++ s' ++ "\""
-    escapeQuotes s'' = concatMap (\c -> if c == '"' then "\"\"" else [c]) s''
+    escapeQuotes = concatMap (\c -> if c == '"' then "\"\"" else [c])
 
 
 classFromSect s = unwords . words . innerText . drop 1  $ take 2 s
@@ -44,16 +77,16 @@ getDataHeadings th = escapeCSV . unwords $ filter (not . null) $ map (filter (no
 -- Similar to getRowData but this only applies to a single string value.
 getText t = trimEnd . unwords $ map fromTagText $ filter isTagText t
 
-getRowData row = map (escapeCSV . trimEnd . unwords . (map ((fromTagText)))) $ map (filter isTagText) row
+getRowData row = map (escapeCSV . trimEnd . unwords . (map fromTagText)) $ map (filter isTagText) row
 
 getValuesFromResult :: [Tag [Char]] -> ([String], [[String]])
 getValuesFromResult r =
   let
-    block = takeWhile (~/= "</table>") r
-    rows = partitions (~== "<tr>") block
-    th = partitions (~== "<th>") $ rows !! 0
+    block = takeWhile (~/= ("</table>" :: String)) r
+    rows = partitions (~== ("<tr>" :: String)) block
+    th = partitions (~== ("<th>" :: String)) $ head rows
     dataHeadings = map getDataHeadings th
-    tdPart = map (partitions (~== "<td>")) $ drop 1 rows
+    tdPart = map (partitions (~== ("<td>" :: String))) $ drop 1 rows
     dataValues = map getRowData tdPart
   in
     (dataHeadings, dataValues)  -- (dataHeadings, map getRowData tdPart)
@@ -68,25 +101,62 @@ getResults r =
 
 printTable table = putStr (unlines (map (intercalate ",") table))
 
+getRaceInfo :: [Tag String] -> [[Char]]
 getRaceInfo t =
   let
-    block = takeWhile (~/= "</td>") $  dropWhile (~/= "<p class=racetitle>") t
-    ps = take 3 $ partitions (~== "<p>") block
+    block = takeWhile (~/= ("</td>" :: String)) $  dropWhile (~/= ("<p class=racetitle>" :: String)) t
+    ps = take 3 $ partitions (~== ("<p>" :: String)) block
   in
     map (escapeCSV . trimEnd . innerText) ps
 
 
-getUrl regatta series year race =
-  "https://race.styc.org/race_info/" ++ r ++ "/" ++ s ++ "/" ++ y ++ "/" ++ "race" ++ race ++ ".htm"
+-- getUrl :: Text -> Text -> Day -> Text -> String
+-- getUrl regatta series date race =
+
+getEvtUrl :: Event -> String
+getEvtUrl Event {regatta = r, series = s, start_date = d, race =n} =
+  raceInfo ++ regatta' ++ "/" ++ series' ++ "/" ++ year ++ "/" ++ "race" ++ race' ++ ".htm"
   where
-    r = map (\x -> if x == ' ' then '_' else x) regatta
-    s  = "Series" ++ series
-    y = show year
+    raceInfo = "https://race.styc.org/race_info/"
+    regatta' = map (\x -> if x == ' ' then '_' else x) $ unpack r
+    (y, _, _) = toGregorian d
+    year = show y
+    series'  = "Series" ++ unpack s
+    race' = unpack n
+
+printUrls :: String -> IO ()
+printUrls = putStrLn
+
+
+
+-- TODO: convert event to url
+
+
+getEvtUrl' Event { regatta = "Ballard Cup" } =
+  "BallardCup"
+
+
+getEvtUrl' Event { regatta = "Foo" } =
+  "Some Other Regatta"
+
+
+-- Only process events that have already occoured.
+runPipeline :: V.Vector Event -> IO ()
+runPipeline events = do
+  currentDay <- utctDay <$> getCurrentTime
+  let urls = V.map getEvtUrl $ V.filter (\e -> start_date e < currentDay) events
+  V.forM_ urls print
 
 main :: IO ()
 main = do
   do
-    putStrLn $ getUrl "Ballard Cup" "III" 2026 "1"
+    let schedulePath = "/Users/gdowding/git/github/gdowding/crewd/schedule.csv"
+    csvData <- BL.readFile schedulePath
+    case decodeByName csvData of
+      Left err -> Exit.die err
+      Right(_, v) -> runPipeline (v :: V.Vector Event)
+
+    -- putStrLn $ getUrl "Ballard Cup" "III" 2026 "1"
 
     -- contents <- readFile "/Users/gdowding/git/github/gdowding/crewd/results/race2.htm"
     -- let tags = parseTags contents
